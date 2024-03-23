ea5baebce0320a90c16d48c99fe94b832346b5bc
pengzhanbo <volodymyr@foxmail.com>
2024-03-23 16:36:10 +0800

    chore: update test to `vitest`

---
+++ test/enhance.test.ts
@@ -0,0 +1,21 @@
+import { beforeEach, it } from 'vitest'
+import { toSimplified, toTraditional } from '../src/index'
+import { setupEnhance } from '../src/enhance'
+
+beforeEach(() => {
+  setupEnhance()
+})
+
+it('enhance converter', ({ expect }) => {
+  const s = '主人何为言少钱，径须酤取对君酌。'
+  const res = '主人何爲言少錢，徑須酤取對君酌。'
+  expect(toSimplified(res, true)).toBe(s)
+  expect(toTraditional(s, true)).toBe(res)
+})
+
+it('phrases', ({ expect }) => {
+  const s = '王妃后来成为了王后'
+  const res = '王妃後來成爲了王后'
+  expect(toSimplified(res, true)).toBe(s)
+  expect(toTraditional(s, true)).toBe(res)
+})
